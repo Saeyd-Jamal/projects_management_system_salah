93c8f9ee2feaa32a33986d3a5429e5e233707250
Saeyd-Jamal <79516145+Saeyd-Jamal@users.noreply.github.com>
2024-10-02 09:34:35 +0300

    update 1.5 --

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,107 +2,110 @@
     <x-slot:breadcrumb>
         <li><a href="#">الرئيسية</a></li>
     </x-slot:breadcrumb>
-
-    {{-- <div class="row">
-        <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
-            <div class="card">
-                <div class="card-body p-3">
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="numbers">
-                                <p class="text-sm mb-0 text-capitalize font-weight-bold">أموال اليوم</p>
-                                <h5 class="font-weight-bolder mb-0">
-                                    $53,000
-                                    <span class="text-success text-sm font-weight-bolder">+55%</span>
-                                </h5>
-                            </div>
+    <h2 class="mt-4">الإحصائيات</h2>
+    <div class="row mt-4">
+        <div class="col-md-4 mb-4">
+            <div class="card shadow bg-info">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <span class="h2 mb-0 ">
+                                {{ App\Models\Allocation::count() }}
+                            </span>
+                            <p class=" mb-0 text-white">عدد التخصيصات</p>
+                            {{-- <span class="badge badge-pill badge-success">+15.5%</span> --}}
                         </div>
-                        <div class="col-4 text-start">
-                            <div
-                                class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                            </div>
+                        <div class="col-auto ">
+                            <span class="fe fe-32 fe-trello text-white mb-0"></span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
-            <div class="card">
-                <div class="card-body p-3">
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="numbers">
-                                <p class="text-sm mb-0 text-capitalize font-weight-bold">مستخدمو اليوم</p>
-                                <h5 class="font-weight-bolder mb-0">
-                                    2,300
-                                    <span class="text-success text-sm font-weight-bolder">+33%</span>
-                                </h5>
-                            </div>
+        <div class="col-md-4 mb-4">
+            <div class="card shadow bg-success">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <span class="h2 mb-0">
+                                {{ App\Models\Executive::count() }}
+                            </span>
+                            <p class=" text-white mb-0">عدد التنفيذات</p>
+                            {{-- <span class="badge badge-pill badge-success">+15.5%</span> --}}
                         </div>
-                        <div class="col-4 text-start">
-                            <div
-                                class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
-                            </div>
+                        <div class="col-auto">
+                            <span class="fe fe-32 fe-watch text-white mb-0"></span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
-            <div class="card">
-                <div class="card-body p-3">
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="numbers">
-                                <p class="text-sm mb-0 text-capitalize font-weight-bold">عملاء جدد</p>
-                                <h5 class="font-weight-bolder mb-0">
-                                    +3,462
-                                    <span class="text-danger text-sm font-weight-bolder">-2%</span>
-                                </h5>
-                            </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-6 col-xl-3 mb-4">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-3 text-center">
+                            <span class="circle circle-sm bg-primary">
+                                <i class="fe fe-16 fe-bar-chart-2 text-white mb-0"></i>
+                            </span>
                         </div>
-                        <div class="col-4 text-start">
-                            <div
-                                class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
-                            </div>
+                        <div class="col">
+                            <p class="small text-muted mb-0">عدد المؤسسات</p>
+                            <span class="h3 mb-0">
+                                {{ count(App\Models\Allocation::select('broker_name')->distinct()->pluck('broker_name')->toArray()) }}
+                            </span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <div class="card-body p-3">
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="numbers">
-                                <p class="text-sm mb-0 text-capitalize font-weight-bold">مبيعات</p>
-                                <h5 class="font-weight-bolder mb-0">
-                                    $103,430
-                                    <span class="text-success text-sm font-weight-bolder">+5%</span>
-                                </h5>
-                            </div>
+        <div class="col-md-6 col-xl-3 mb-4">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-3 text-center">
+                            <span class="circle circle-sm bg-primary">
+                                <i class="fe fe-16 fe-activity text-white mb-0"></i>
+                            </span>
                         </div>
-                        <div class="col-4 text-start">
-                            <div
-                                class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
-                            </div>
+                        <div class="col">
+                            <p class="small text-muted mb-0">عدد المشاريع</p>
+                            <span class="h3 mb-0">
+                                {{ count(App\Models\Allocation::select('project_name')->distinct()->pluck('project_name')->toArray()) }}
+                            </span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div> --}}
-    <h2>العام</h2>
+        <div class="col-md-6 col-xl-3 mb-4">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-3 text-center">
+                            <span class="circle circle-sm bg-primary">
+                                <i class="fe fe-16 fe-users text-white mb-0"></i>
+                            </span>
+                        </div>
+                        <div class="col">
+                            <p class="small text-muted mb-0">عدد التجار</p>
+                            <span class="h3 mb-0">
+                                {{ count(App\Models\Executive::select('account')->distinct()->pluck('account')->toArray()) }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h2 class="mt-4">العام</h2>
     <div class="row mt-4">
         <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
             <div class="card">
                 <div class="card-body p-3">
-                    <a href="{{route('accreditations.create')}}" class="d-flex flex-column align-items-center p-3">
+                    <a href="{{ route('accreditations.create') }}" class="d-flex flex-column align-items-center p-3">
                         <i class="fe fe-plus" style="font-size: 65px;"></i>
                         <h3> إضافة مشروع جديد</h3>
                     </a>
@@ -110,5 +113,7 @@
             </div>
         </div>
     </div>
+
+
 
 </x-front-layout>
